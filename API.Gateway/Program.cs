50898c0af4204b5f0c6dd9534c289af6e26ab000
Kioku Sensei <antoni.filonau@gmail.com>
2024-02-09 01:15:16 +0100

    Configure Ocelot gateway

--- API.Gateway/Program.cs
+++ API.Gateway/Program.cs
@@ -3,21 +3,11 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.MapControllers();
-
 await app.UseOcelot();
-
 app.Run();
